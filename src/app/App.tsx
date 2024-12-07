0fdc462c0787ecdb6cd96203d460ab1d46707247
MeNoobly <84956743974@mail.ru>
2024-12-07 20:28:21 +0300

    feature: scss modules and mui integration

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@mui/material'
 import { FC } from 'react'
 
 const App: FC = () => {
-    return <div>App</div>
+    return (
+        <div>
+            <Button variant="contained" color="primary">
+                123
+            </Button>
+        </div>
+    )
 }
 
 export default App
